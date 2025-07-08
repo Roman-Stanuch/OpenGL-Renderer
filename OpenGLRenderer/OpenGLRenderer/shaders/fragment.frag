2e9aa143a73a4695ff84f639080ae5835dad89de
Polygonal Graphics <154336865+Roman-Stanuch@users.noreply.github.com>
2025-07-08 06:57:12 -0500

    Added texture loading

--- OpenGLRenderer/OpenGLRenderer/shaders/fragment.frag
+++ OpenGLRenderer/OpenGLRenderer/shaders/fragment.frag
@@ -1,7 +1,12 @@
 #version 460 core
 out vec4 FragColor;
 
-void main()
-{
-    FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+in vec3 ourColor;
+in vec2 texCoord;
+
+layout (binding = 0) uniform sampler2D texture1;
+layout (binding = 1) uniform sampler2D texture2;
+
+void main() {
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
 }
--- OpenGLRenderer/OpenGLRenderer/source/main.cpp
+++ OpenGLRenderer/OpenGLRenderer/source/main.cpp
@@ -1,5 +1,6 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "stb/stb_image.h"
 #include "Shader.h"
 
 #include <iostream>
@@ -10,6 +11,7 @@ constexpr uint32_t SCREEN_HEIGHT = 600;
 
 GLFWwindow* InitalizeWindow();
 GLuint GetTriangleVAO();
+GLuint GetTexture(const char* source, bool flip = true);
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window);
 
@@ -20,6 +22,8 @@ int main() {
 	}
 
 	GLuint triangleVAO = GetTriangleVAO();
+	GLuint crateTexture = GetTexture("textures/container.jpg");
+	GLuint awesomeFaceTexture = GetTexture("textures/awesomeface.png");
 	Shader shaderProgram("shaders/vertex.vert", "shaders/fragment.frag");
 
 	while (!glfwWindowShouldClose(window)) {
@@ -31,6 +35,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		shaderProgram.Use();
 		glBindVertexArray(triangleVAO);
+		glBindTextureUnit(0, crateTexture);
+		glBindTextureUnit(1, awesomeFaceTexture);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Swap Buffers and Poll Events
@@ -64,10 +70,11 @@ GLFWwindow* InitalizeWindow() {
 
 GLuint GetTriangleVAO() {
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
 	unsigned int indices[] = {
@@ -89,10 +96,38 @@ GLuint GetTriangleVAO() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
 	return VAO;
+}
+
+GLuint GetTexture(const char* source, bool flip) {
+	int width;
+	int height;
+	int numberOfChannels;
+	stbi_set_flip_vertically_on_load(flip);
+	unsigned char* data = stbi_load(source, &width, &height, &numberOfChannels, 0);
+	if (data) {
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		GLuint textureType = (numberOfChannels == 3) ? GL_RGB : GL_RGBA;
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, textureType, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		return texture;
+	}
+	else {
+		std::cout << "ERROR: Failed to load texture\n";
+	}
+
+	return 0;
 }
 
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
